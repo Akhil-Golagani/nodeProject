75207038b74d766985c46639ea7c6633869019d1
Akhil Golagani <akhilgolagani528@gmail.com>
2026-02-09 11:49:49 +0530

    Exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello to the server 9966");
+app.get("/user", (req, res) => {
+    res.send({firstName: "Akhil", lastName : "Golagani"})
+});
+
+app.post("/user", (req, res) => {
+    res.send("User created successfully");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("User deleted successfully");
 })
 
-app.use("/",(req,res)=>{
-    res.send("Welcome to the server port 9966");
-})
+app.use("/test",(req,res)=>{
+    res.send("Hello to the server 9966");
+});
 
 app.listen(9966, () => {
     console.log("Server is listening to 9966");
