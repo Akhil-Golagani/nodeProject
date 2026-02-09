7159399643ad5a2e586952dc39d4a5e75ca15a7f
Akhil Golagani <akhilgolagani528@gmail.com>
2026-02-09 13:25:32 +0530

    Exploring the Routing techniques

--- src/app.js
+++ src/app.js
@@ -2,6 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.get(/^\/a(b)?c$/, (req, res) => {
+    res.send({firstName : "Akhil", lastName : "Golagani"});
+});
+
+app.get("/user", (req, res) => {
+    console.log(req.query);
+    res.send({firstName: "Akhil", lastName : "Golagani"})
+});
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.query);
+    console.log(req.params);
+    res.send({firstName: "Akhil", lastName : "Golagani"})
+});
+
+
 app.get("/user", (req, res) => {
     res.send({firstName: "Akhil", lastName : "Golagani"})
 });
